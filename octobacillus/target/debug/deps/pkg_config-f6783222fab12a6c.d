a2892330c4e7c2363356d637d7b61148fa04d7ba
ekahPruthvi <p1sribhat@gmail.com>
2026-01-28 09:19:47 +0530

    niri location in var

---
+++ octobacillus/target/debug/deps/pkg_config-f6783222fab12a6c.d
@@ -0,0 +1,7 @@
+/home/ekah/octobacillus/octobacillus/target/debug/deps/pkg_config-f6783222fab12a6c.d: /home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pkg-config-0.3.32/src/lib.rs
+
+/home/ekah/octobacillus/octobacillus/target/debug/deps/libpkg_config-f6783222fab12a6c.rlib: /home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pkg-config-0.3.32/src/lib.rs
+
+/home/ekah/octobacillus/octobacillus/target/debug/deps/libpkg_config-f6783222fab12a6c.rmeta: /home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pkg-config-0.3.32/src/lib.rs
+
+/home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pkg-config-0.3.32/src/lib.rs:
---
+++ octobacillus/target/debug/deps/unicode_ident-f8e7225836952946.d
@@ -0,0 +1,8 @@
+/home/ekah/octobacillus/octobacillus/target/debug/deps/unicode_ident-f8e7225836952946.d: /home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
+
+/home/ekah/octobacillus/octobacillus/target/debug/deps/libunicode_ident-f8e7225836952946.rlib: /home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
+
+/home/ekah/octobacillus/octobacillus/target/debug/deps/libunicode_ident-f8e7225836952946.rmeta: /home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
+
+/home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs:
+/home/ekah/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs:
